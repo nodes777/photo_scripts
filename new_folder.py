df75cde0aa244c9ff54c498bf81ad4a265570b5a
Taylor Nodell <taylor.nodell@gmail.com>
2021-11-13 11:05:45 +1100

    inital commit, moves files from SD card, creates new folders

---
+++ new_folder.py
@@ -0,0 +1,46 @@
+import os
+import sys
+from datetime import datetime
+from pathlib import Path
+import shutil
+
+camera_path = "E:/DCIM/108D3400"
+photos_path = "D:\Photos"
+keepers_path = "D:\Photos\Keepers"
+descriptions_template_path = "D:\Photos\Keepers\descriptions_template.ods"
+todays_date =  datetime.today().strftime('%Y-%m-%d')
+
+
+def make_new_folder(folder_name_input):
+    new_folder_name = folder_name_input + "_" + todays_date
+    new_folder = os.path.join(photos_path,new_folder_name)
+    os.makedirs(new_folder,exist_ok=False)
+    print(f'Created New Folder {new_folder}')
+    return new_folder, new_folder_name
+
+def move_photos_from_camera_to_comp(camera_path,new_folder):
+    files_on_camera = os.listdir(camera_path)
+    num_of_files = len(files_on_camera)
+    # TODO: If the moving of files fails, delete the new_folder that was created
+    for idx, file in enumerate(files_on_camera):
+        shutil.move(camera_path+"/"+file, new_folder)
+        # end="\r" print to start where previous one ended, to remove previous line in terminal
+        print(f"Moving {file} {idx}/{num_of_files}", end="\r")
+        
+
+def create_keepers_folder(new_folder_name):
+    # Create the same folder name, under /Keepers
+    new_keepers_folder = os.path.join(keepers_path,new_folder_name)
+    os.makedirs(new_keepers_folder,exist_ok=False)
+    print(f'Created New Folder {new_folder}') 
+    # Copy a descriptions file from template
+    descriptions_file = shutil.copy(descriptions_template_path,new_keepers_folder)
+    os.startfile(descriptions_file)
+
+
+
+new_folder, new_folder_name = make_new_folder(sys.argv[1])
+
+move_photos_from_camera_to_comp(camera_path,new_folder)
+
+create_keepers_folder(new_folder_name)
