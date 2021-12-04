b8c0c2cc8b771ae90a4aad076c5fe48bb044d770
nodes777 <nodes777@gmail.com>
2021-12-04 20:37:23 +1100

    renames descriptions.xlsx, puts move_photos into functions

--- move_photos.py
+++ move_photos.py
@@ -5,76 +5,62 @@ from pathlib import Path
 import shutil
 import numpy as np
 import pandas as pd
+import time
 
-
+start_time = time.time()
 photos_path = "D:\Photos"
 keepers_path = "D:\Photos\Keepers"
-descriptions_template_path = "D:\Photos\Keepers\descriptions_template.ods"
-
-# DSC_0001.JPG
-
 file_prefix = "DSC_"
 file_suffixes = [".JPG",".NEF",".MOV"]
 delimeters = [";",","]
 
-temp_descriptions_path = "D:\Photos\Keepers\La_Perouse_2021-11-14\descriptions_template.xlsx"
+#  prefixing with "r" produces a raw string
+temp_descriptions_path = r"D:\Photos\Keepers\bundeena_to_royal_national_park_2021-11-28\descriptions_template.xlsx"
 temp_initial_path = temp_descriptions_path.replace("Keepers\\","").replace("descriptions_template.xlsx","")
 temp_destination_path = temp_descriptions_path.replace("descriptions_template.xlsx","")
 
 # Need path to descriptions.ods in keepers
 # Need the name of new_keepers_folder from prev flow
+def read_sheet_and_move_files(descriptions_path):
+    descriptions_df = pd.read_excel(descriptions_path)
+    print(descriptions_df.head())
 
-descriptions_df = pd.read_excel(temp_descriptions_path)
-print(descriptions_df.head())
-
-file_names_to_move = []
+    file_names_to_move = []
     
-# Loop through each row
-# For the text in each Photo Numbers
-# Take each number per delimiter
-for index, row in descriptions_df.iterrows():
-    photo_nums_str = row['Photo Numbers']
+    for index, row in descriptions_df.iterrows():
+        photo_nums_str = row['Photo Numbers']
 
-    # Q: NaN is a float for some reason - WHY?
-    if not isinstance(photo_nums_str, float):
-        # QUESTION: Why isnt this splitting on the ;?
-        # photo_nums = re.split('; |,', photo_nums_str)
+        # Q: NaN is a float for some reason - WHY?
+        if not isinstance(photo_nums_str, float):
+            # QUESTION: What is the diff between an iterator and a list? - I think of these both as arrays
+            photo_nums = list(filter(None,str(photo_nums_str).replace(",",";").split(";")))
 
-        # replace then split b/c above doesn't work
-        # filter does a filter, returns an iterator, list() puts it in a list
-        # QUESTION: What is the diff between an iterator and a list? - I think of these both as arrays
-        photo_nums = list(filter(None, photo_nums_str.replace(",",";").split(";")))
+            # Create 4 digit value for each num b/c thats the file format
+            for i, photo_num in enumerate(photo_nums):
+                # Prepend the 0s in the file name
+                tmp =  "000" + photo_num
+                # Keep only last 4 chars
+                # list[start_bound: ending_bound]
+                file_num = "".join(list(tmp)[-4:])
+                file_name = file_prefix + file_num
 
-        # Create 4 digit value for each num b/c thats the file format
-        # Q: Is there a better way to match the file names to the nums?
+                for j, file_suffix in enumerate(file_suffixes):
+                    file_names_to_move.append(file_name + file_suffix)
+                    
+    # For each file_names_to_move, try to move each kind of file
+    for idx, file_name_to_move in enumerate(file_names_to_move):
+        file_in_path = temp_initial_path + file_name_to_move 
+        if(os.path.isfile(file_in_path)):
+            shutil.move(file_in_path, temp_destination_path)
+            # end="\r" print to start where previous one ended, to remove previous line in terminal
+            print(f"Moving {file_in_path} {idx}/{round(len(file_names_to_move)-1/2)}", end="\r")
 
-        for i, photo_num in enumerate(photo_nums):
-            # Prepend the  0s in the file name
-            tmp =  "000" + photo_num
-            # Keep only last 4 chars
-            # list[start_bound: ending_bound]
-            file_num = "".join(list(tmp)[-4:])
-            file_name = file_prefix + file_num
-            # TODO sometimes .NEF files or .MOV files won't exist. 
-            # Should I add them here or do logic to filter somewhere before trying to move
-            file_names_to_move.append(file_name + ".JPG")
-            file_names_to_move.append(file_name + ".NEF")
-            file_names_to_move.append(file_name + ".MOV")
-
-
-# For each file_names_to_move, try to move each kind of file
-for idx, file_name_to_move in enumerate(file_names_to_move):
-    file_in_path = temp_initial_path + file_name_to_move 
-    if(os.path.isfile(file_in_path)):
-        shutil.move(file_in_path, temp_destination_path)
-        # end="\r" print to start where previous one ended, to remove previous line in terminal
-        print(f"Moving {file_in_path} {idx}/{round(len(file_names_to_move)-1/2)}", end="\r")
-
+read_sheet_and_move_files(temp_descriptions_path)
 print("\n Done!")
+print("--- %s seconds ---" % (time.time() - start_time))
 
-# TODO: Put these into functions
 # TODO: Figure out sleep from new_folder.py
 # TODO: Delete NEF files that aren't moved
 # TODO: Check if all iNat paths are unique - Send warning if not
 # TODO: Create an intializer function that creates a config file for specific paths for camera path and photos path
-# TODO: Rename the descriptions_template.xlsx to just descriptions
+# TODO: Allow for ranges of photo nums
--- new_folder.py
+++ new_folder.py
@@ -11,6 +11,7 @@ camera_path = "E:/DCIM/108D3400"
 photos_path = "D:\Photos"
 keepers_path = "D:\Photos\Keepers"
 descriptions_template_path = "D:\Photos\Keepers\descriptions_template.xlsx"
+descriptions_file_name = "descriptions.xlsx"
 todays_date =  datetime.today().strftime('%Y-%m-%d')
 
 MissingInputError = "Error No Folder Name Provided"
@@ -24,7 +25,7 @@ def make_new_folder(folder_name_input):
     print(f'Created New Folder {new_folder}')
     return new_folder, new_folder_name
 
-def move_photos_from_camera_to_comp(camera_path,new_folder):
+def move_photos_from_camera_to_comp(camera_path, new_folder):
     files_on_camera = os.listdir(camera_path)
     num_of_files = len(files_on_camera)
     # TODO: If the moving of files fails, delete the new_folder that was created
@@ -36,16 +37,17 @@ def move_photos_from_camera_to_comp(camera_path,new_folder):
 
 def create_keepers_folder(new_folder_name):
     # Create the same folder name, under /Keepers
-    new_keepers_folder = os.path.join(keepers_path,new_folder_name)
-    # TODO Rename descriptions_tempplate to descriptions
+    new_keepers_folder = os.path.join(keepers_path, new_folder_name)
 
-    os.makedirs(new_keepers_folder,exist_ok=False)
-    print(f'Created New Folder {new_folder}') 
+    os.makedirs(new_keepers_folder, exist_ok=False)
+    print(f'Created New Folder {new_keepers_folder}') 
     # Copy a descriptions file from template
-    descriptions_file = shutil.copy(descriptions_template_path,new_keepers_folder)
+    descriptions_copy = shutil.copy(descriptions_template_path, new_keepers_folder)
+    # Remove the "template" part of the name
+    descriptions_file = new_keepers_folder + "/" + descriptions_file_name
+    os.rename(descriptions_copy, descriptions_file)
     os.startfile(descriptions_file)
-    # TODO: open first image
-
+    os.startfile(new_folder_name)
     return descriptions_file
 
 
